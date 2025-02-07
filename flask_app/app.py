3f1f3fdd61dde541fe6c307cc9cd0262e962e1c0
aasha25 <114113137+aasha25@users.noreply.github.com>
2025-02-07 23:48:46 +0000

    nes

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,12 +4,11 @@ import random
 app = Flask(__name__)
 
 images = [
-    
-    "https://i.pinimg.com/474x/c1/f7/2f/c1f72f23c2b299fd556bf35c0f991bbe.jpg",
-    "https://i.pinimg.com/474x/4c/1d/be/4c1dbed508d2e85c1488b576939cf0e6.jpg",
-    "https://i.pinimg.com/originals/c4/8e/c8/c48ec805ab6adb7738c5551bea137939.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Captain_America%27s_shield.svg/640px-Captain_America%27s_shield.svg.png",
-    "https://www.nicepng.com/png/detail/120-1208220_black-widow-symbol-fill-black-widow-logo-marvel.png"
+
+    "https://images.app.goo.gl/hYaMr6duS4SxKTX46",
+    "https://images.app.goo.gl/93eSRJZ9tHC6kosp6",
+    "https://images.app.goo.gl/HfiRGavhtsM5NBif6",
+    "https://images.app.goo.gl/iFobbytq4iRAU9La8"
 ]
 
 @app.route('/')
