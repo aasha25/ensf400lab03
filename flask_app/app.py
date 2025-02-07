6fbca122cc0b4189a7d3806ad59986b38ef6b8ea
aasha25 <114113137+aasha25@users.noreply.github.com>
2025-02-07 23:15:23 +0000

    add static

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,8 +4,7 @@ import random
 app = Flask(__name__)
 
 images = [
-    "https://www.kindpng.com/picc/m/343-3437091_iron-man-helmet-logo-hd-png-download.png",
-    "https://i.ebayimg.com/images/g/aTwAAOxyVaBS3dUJ/s-l400.jpg",
+    
     "https://i.pinimg.com/474x/c1/f7/2f/c1f72f23c2b299fd556bf35c0f991bbe.jpg",
     "https://i.pinimg.com/474x/4c/1d/be/4c1dbed508d2e85c1488b576939cf0e6.jpg",
     "https://i.pinimg.com/originals/c4/8e/c8/c48ec805ab6adb7738c5551bea137939.jpg",
